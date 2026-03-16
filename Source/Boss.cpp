8f211f2dd72f2f70efbe0850667e7bf05347a35c
Haimen1505 <116255011+Haimen1505@users.noreply.github.com>
2026-03-16 11:07:09 +0800

    Fixed faulty call of FSM initialization
    Fixed initialization of turret boss FSM to call correct number of parameters.

--- Source/Boss.cpp
+++ Source/Boss.cpp
@@ -15,7 +15,7 @@ Boss::Boss(std::string enemyName, f32 enemyHealth, f32 enemyDamage, TexturedSpri
 	if (enemyName == "Boss 2") {
 		std::cout << "true\n";
 		bossStateMachine = std::make_unique<Boss2_FSM>(this, attackData[0].attackDamage, attackData[0].attackStartup, attackData[0].attackInterval, attackData[0].attackEndlag,
-			attackData[1].attackDamage, attackData[1].attackStartup, attackData[1].attackInterval, attackData[1].attackEndlag);
+			attackData[1].attackDamage, 0.0f, attackData[1].attackStartup, attackData[1].attackInterval, attackData[1].attackEndlag);
 		std::cout << typeid(bossStateMachine).name() << '\n';
 	}
 }
