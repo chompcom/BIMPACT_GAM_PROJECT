6508f6a0e3c04d495f82fc910a665da3ee8b0fce
HQGitter <69576378+hqgitter@users.noreply.github.com>
2026-04-05 21:48:42 +0800

    Added Filters finally

--- Source/RoomData.cpp
+++ Source/RoomData.cpp
@@ -1,6 +1,21 @@
+/* Start Header ************************************************************************/
+/*!
+\file		RoomData.cpp
+\author 	Hong Josiah Qin, hong.j, 2501239
+\par  		hong.j@digipen.edu
+\brief		Only contains the default constructor to room data.
+
+Copyright (C) 2026 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents
+without the prior written consent of DigiPen Institute of
+Technology is prohibited.
+*/
+/* End Header **************************************************************************/
+
 #include "RoomData.h"
 #include "Loaders/DataLoader.h"
 
+// Gets the DataLoader's mesh location for the particle system
 RoomData::RoomData() : player{ nullptr }, enemyList{}, giftList{}, boss{ nullptr }, grid{} 
 ,particleSystem{DataLoader::GetMesh()}
 {
